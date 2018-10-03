2d0f289e2f8e9f18a1c6278e2ebd9204537caa91
Eric Lortie <e@ericlortie.com>
2018-10-03 20:46:05 -0300

    Removng margin from top of site.

--- wp-content/themes/tempestgrove/functions.php
+++ wp-content/themes/tempestgrove/functions.php
@@ -151,6 +151,10 @@ function wp_smarty(){
 
 // Not applicable for this project
 
+function remove_admin_login_header() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'remove_admin_login_header');
 
 /* ************************************************************************ **
 ** WP Admin Cleanup                                                         **
