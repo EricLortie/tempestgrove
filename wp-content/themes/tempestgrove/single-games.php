6ab67bf6fda2e12890bb04373ff5ad12f4c607ee
Eric Lortie <e@ericlortie.com>
2018-10-12 21:47:55 -0300

    Tweaks to alert css to make it look nicer.

--- wp-content/themes/tempestgrove/single-games.php
+++ wp-content/themes/tempestgrove/single-games.php
@@ -3,8 +3,8 @@
 // Add Page Javascripts
 add_action('wp_enqueue_scripts', 'add_page_javascripts');
 function add_page_javascripts(){
-	wp_enqueue_script('slick',     	ASSETS_DIR . 'vendor/slick.js',                  	array(), ASSET_VERSION, 1);
-	wp_enqueue_script('forms',		ASSETS_DIR . 'js/components/forms/forms.js', 		array(), ASSET_VERSION, 1);
+	wp_enqueue_script('slick',    ASSETS_DIR . 'vendor/slick.js',                  	array(), ASSET_VERSION, 1);
+	wp_enqueue_script('forms',		ASSETS_DIR . 'js/components/forms/forms.js', 		  array(), ASSET_VERSION, 1);
 	wp_enqueue_script('dialog',		ASSETS_DIR . 'js/components/dialog/dialog.js', 		array(), ASSET_VERSION, 1);
 	wp_enqueue_script('gallery',	ASSETS_DIR . 'js/components/gallery/gallery.js',	array(), ASSET_VERSION, 1);
 }
